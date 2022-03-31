e95a786ac4f87446c946fb3227868c0baa81b07b
markohpolo <dhfis48@outlook.com>
2022-03-31 10:01:28 +1000

    added script to remove background from exported frames

--- data_collection/pyVideoExtraction.py
+++ data_collection/pyVideoExtraction.py
@@ -5,13 +5,22 @@ def export_frames(object_name: str, frame_limit: int):
     vidcap = cv2.VideoCapture('3d_models/{}.webm'.format(object_name))
     success,image = vidcap.read()
     count = 0
-    while success and count <= frame_limit:
+    while success and count < frame_limit:
         count += 1
         cv2.imwrite("frames/{}_%d.png".format(object_name) % count, image)
         count += 1
         cv2.imwrite("frames/{}_%d.png".format(object_name) % count, cv2.flip(image, 1))
         success,image = vidcap.read()
     print("{} frames of {} captured".format(count - 1, object_name))
+
+
+# def export_cropped_frame(object_name, counter, image):
+
+# def export_filpped_frame(object_name, counter, image):
+#     cv2.imwrite("frames/{}_%d.png".format(object_name) % counter, cv2.flip(image, 1))
+
+def export_original_frame(object_name, counter, image):
+    cv2.imwrite("frames/{}_%d.png".format(object_name) % counter, image)
 
 def main():
     _, _, object_names = next(walk("./3d_models"), (None, None, []))
