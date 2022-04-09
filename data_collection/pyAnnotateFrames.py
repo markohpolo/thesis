cef11054030e9faab33261cabb8c0b89cd13ccdd
markohpolo <dhfis48@outlook.com>
2022-04-09 17:09:18 +1000

    fixed bugs

--- data_collection/pyAnnotateFrames.py
+++ data_collection/pyAnnotateFrames.py
@@ -1,7 +1,11 @@
 import cv2
+from cv2 import normalize
 import tqdm
 import concurrent.futures
 from os import walk
+
+OBJECT = "vayne_run" # object name
+OBJECT_ID = 0 # object id
 
 NUM_WORKERS = 50
 BACKGROUND = [58, 246, 142] # GREEN
@@ -13,99 +17,75 @@ TOLERANCE_OFFSET_2_MAX = BACKGROUND[1] + 1.0 * TOLERANCE
 TOLERANCE_OFFSET_3_MIN = BACKGROUND[2] - 2.5 * TOLERANCE # Teemo viewer: 2.5
 TOLERANCE_OFFSET_3_MAX = BACKGROUND[2] + 2.5 * TOLERANCE
 
-# BLACK_OFFSET_1_MIN = 0
-# BLACK_OFFSET_1_MAX = 0 + 1.0 * TOLERANCE
-# BLACK_OFFSET_2_MIN = 0
-# BLACK_OFFSET_2_MAX = 0 + 1.0 * TOLERANCE
-# BLACK_OFFSET_3_MIN = 0
-# BLACK_OFFSET_3_MAX = 0 + 2.5 * TOLERANCE
-
-# WHITE_OFFSET_1_MIN = 255 - 1.0 * TOLERANCE
-# WHITE_OFFSET_1_MAX = 255
-# WHITE_OFFSET_2_MIN = 255 - 1.0 * TOLERANCE
-# WHITE_OFFSET_2_MAX = 255
-# WHITE_OFFSET_3_MIN = 255 - 2.5 * TOLERANCE
-# WHITE_OFFSET_3_MAX = 255
-
-
-# def is_mouse_pixel(pixel):
-#     if pixel[0] > WHITE_OFFSET_1_MIN and pixel[0] < WHITE_OFFSET_1_MAX \
-#         and pixel[1] > WHITE_OFFSET_2_MIN and pixel[1] < WHITE_OFFSET_2_MAX \
-#         and pixel[2] > WHITE_OFFSET_3_MIN and pixel[2] < WHITE_OFFSET_3_MAX:
-#         return True
-
-#     elif pixel[0] > BLACK_OFFSET_1_MIN and pixel[0] < BLACK_OFFSET_1_MAX \
-#         and pixel[1] > BLACK_OFFSET_2_MIN and pixel[1] < BLACK_OFFSET_2_MAX \
-#         and pixel[2] > BLACK_OFFSET_3_MIN and pixel[2] < BLACK_OFFSET_3_MAX:
-#         return True
-
-#     return False
-
-def pixel_not_green(pixel):
-    if not(pixel[0] > TOLERANCE_OFFSET_1_MIN and pixel[0] < TOLERANCE_OFFSET_1_MAX \
-        and pixel[1] > TOLERANCE_OFFSET_2_MIN and pixel[1] < TOLERANCE_OFFSET_2_MAX \
-        and pixel[2] > TOLERANCE_OFFSET_3_MIN and pixel[2] < TOLERANCE_OFFSET_3_MAX):
-        return True
-
-    return False
-
 def run_worker(frame_file):
-    original_image = cv2.imread("frames/vayne_run/{}".format(frame_file))
+    original_image = cv2.imread("frames/{}/{}".format(OBJECT, frame_file))
     rotate_1 = cv2.rotate(original_image, cv2.cv2.ROTATE_90_CLOCKWISE)
-    rotate_2 = cv2.rotate(rotate_1, cv2.cv2.ROTATE_90_CLOCKWISE)
-    rotate_3 = cv2.rotate(rotate_2, cv2.cv2.ROTATE_90_CLOCKWISE)
 
     image_length = len(original_image[0])
     image_width = len(original_image)
 
-    top_y = get_coordinate(original_image)
-    left_x =  get_coordinate(rotate_1)
-    bottom_y = image_width - get_coordinate(rotate_2)
-    right_x= image_length - get_coordinate(rotate_3)
+    top_y, bottom_y = get_coordinates(original_image, image_width // 2, image_width)
+    left_x, right_x =  get_coordinates(rotate_1, image_length // 2, image_length)
 
-    # sanity check code
-    # image_with_box = cv2.rectangle(original_image, (left_x, top_y), (right_x, bottom_y), (255, 0, 0), 2)
-    # cv2.imwrite("{}_boxed.png".format(frame_file), image_with_box)
     with open("annotations/" + frame_file.replace("png", "txt"), 'w') as f:
-        f.write(str(left_x) + " ")
-        f.write(str(right_x) + " ")
-        f.write(str(top_y) + " ")
-        f.write(str(bottom_y))
+    #with open(frame_file.replace("png", "txt"), 'w') as f:
+        f.write(normalize_frames(left_x, right_x, top_y, bottom_y, image_width, image_length))
 
     return left_x, right_x, top_y, bottom_y
 
+def normalize_frames(left_x, right_x, top_y, bottom_y, image_width, image_length):
+    b_width    = (right_x - left_x)
+    b_height   = (bottom_y - top_y)
+    b_center_x = b_width / 2 + left_x
+    b_center_y = b_height / 2 + top_y
+    
+    # Normalise the co-ordinates by the dimensions of the image
+    b_center_x /= image_length 
+    b_center_y /= image_width 
+    b_width    /= image_length 
+    b_height   /= image_width
+                
+    return "{} {} {} {} {}".format(OBJECT_ID, round(b_center_x, 6), round(b_center_y, 6), round(b_width, 6), round(b_height, 6))
 
-def get_coordinate(image):
-    count = 0
-    for i in range(len(image)):
-        count += 1
-        if count < 100:
-            continue
-        for pixel in image[i]:
-            # if is_mouse_pixel(pixel):
-            #     continue
-            if pixel_not_green(pixel):
-                return i                
-    # 0 on fail
-    return 0
+def is_pixel_green(pixel):
+    if pixel[0] > TOLERANCE_OFFSET_1_MIN and pixel[0] < TOLERANCE_OFFSET_1_MAX \
+        and pixel[1] > TOLERANCE_OFFSET_2_MIN and pixel[1] < TOLERANCE_OFFSET_2_MAX \
+        and pixel[2] > TOLERANCE_OFFSET_3_MIN and pixel[2] < TOLERANCE_OFFSET_3_MAX:
+        return True
+
+    return False
+
+def get_coordinates(image, center, bottom_limit):
+    top = 0
+    bottom = 0
+    # center -> top
+    for i in range(center, 0, -1):
+        if all(is_pixel_green(pixel) for pixel in image[i]):
+            top = i
+            break
+    # center -> bottom
+    for i in range(center, bottom_limit):
+        if all(is_pixel_green(pixel) for pixel in image[i]):
+            bottom = i
+            break
+
+    return top, bottom
     
     
 
 if __name__ == "__main__":
-    # scan all objects
-    _, _, object_names = next(walk("./3d_models"), (None, None, []))
-    for object_name in object_names:
-        object_name = object_name[:-5]
-        if object_name != "vayne_run":
+    _, _, frame_files = next(walk("./frames/{}".format(OBJECT)), (None, None, []))
+    # scan frames
+    with concurrent.futures.ProcessPoolExecutor(max_workers=NUM_WORKERS) as executor:
+        for left_x, right_x, top_y, bottom_y in tqdm.tqdm(executor.map(run_worker, frame_files), "Drawing boxes for {}".format(OBJECT), total=len(frame_files)):
             continue
-        _, _, frame_files = next(walk("./frames/{}".format(object_name)), (None, None, []))
-        # scan frames
-        with concurrent.futures.ProcessPoolExecutor(max_workers=NUM_WORKERS) as executor:
-            for left_x, right_x, top_y, bottom_y in tqdm.tqdm(executor.map(run_worker, frame_files), "Drawing boxes for {}".format(object_name), total=len(frame_files)):
-                continue
-                
-        # for frame_file in frame_files:
-        #     if frame_file != "vayne_run1.png":
-        #         continue
-        #     left_x, right_x, top_y, bottom_y = run_worker(frame_file)
-        #     print(left_x, right_x, top_y, bottom_y)
+            
+    # sanity check code
+    # for frame_file in frame_files:
+    #     if frame_file != "vayne_run2000.png":
+    #         continue
+    #     original_image = cv2.imread("frames/{}/{}".format(OBJECT, frame_file))
+    #     left_x, right_x, top_y, bottom_y = run_worker(frame_file)
+    #     image_with_box = cv2.rectangle(original_image, (left_x, top_y), (right_x, bottom_y), (255, 0, 0), 2)
+    #     cv2.imwrite("{}_boxed.png".format(OBJECT), image_with_box)
+    #     print(left_x, right_x, top_y, bottom_y)
